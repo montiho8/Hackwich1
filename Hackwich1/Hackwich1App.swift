f6d10c5126a2d084dbaefc392338736d88753e37
Dominique Cortez-Montiho <montiho8@hawaii.edu>
2022-08-31 02:12:28 -1000

    Initial Commit

---
+++ Hackwich1/Hackwich1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hackwich1App.swift
+//  Hackwich1
+//
+//  Created by Dominique Cortez-Montiho on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Hackwich1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
